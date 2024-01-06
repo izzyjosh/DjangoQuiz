4eca529bbc2e68df738fe9eec6ae821596ff4fc0
izzyjosh <calebayuba222@gmail.com>
2024-01-06 16:56:41 +0100

    Added annotation to variables

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Quiz(models.Model):
-    name = models.CharField(max_length=100)
+    name:str = models.CharField(max_length=100)
 
     class Meta:
         verbose_name_plural = "Quizzes"
@@ -12,24 +12,24 @@ class Quiz(models.Model):
         return self.name
 
 class Question(models.Model):
-    quiz = models.ForeignKey(Quiz,related_name="questions",on_delete=models.CASCADE)
-    question = models.TextField()
+    quiz:Quiz = models.ForeignKey(Quiz,related_name="questions",on_delete=models.CASCADE)
+    question:str = models.TextField()
 
     def __str__(self):
         return self.question
 
 class Option(models.Model):
-    question = models.ForeignKey(Question,related_name="options",on_delete=models.CASCADE)
-    option = models.CharField(max_length=200)
+    question:User = models.ForeignKey(Question,related_name="options",on_delete=models.CASCADE)
+    option:str = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
 
     def __str__(self):
         return self.option
 
 class UserAnswer(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    question = models.ForeignKey(Question,on_delete=models.CASCADE)
-    selected_option = models.ForeignKey(Option,on_delete=models.CASCADE)
+    user:User = models.ForeignKey(User,on_delete=models.CASCADE)
+    question:Question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    selected_option:Option = models.ForeignKey(Option,on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = "User's Answer"

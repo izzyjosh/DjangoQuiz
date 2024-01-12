c5e07a343aa75049b7afb09b7b9fbbfe3516dd4e
izzyjosh <calebayuba222@gmail.com>
2024-01-12 22:08:03 +0100

    javascript call for next and previous question

--- static/main/js/script.js
+++ static/main/js/script.js
@@ -1,18 +1,17 @@
 let time = document.querySelector(".time");
-  function countdownTimer(durationInSeconds) {
-      let timer = durationInSeconds;
-      const interval = setInterval(() => {
-      const minutes = Math.floor(timer / 60);
-      let seconds = timer % 60;
+function countdownTimer(durationInSeconds) {
+  let timer = durationInSeconds;
+  const interval = setInterval(() => {
+    const minutes = Math.floor(timer / 60);
+    let seconds = timer % 60;
 
-      seconds = seconds < 10 ? '0' + seconds : seconds;
-      time.innerHTML = `${minutes}:${seconds}`;
-      if (--timer < 0) {
-        clearInterval(interval);
-      }
-    }, 1000);
-  }
-
+    seconds = seconds < 10 ? '0' + seconds: seconds;
+    time.innerHTML = `${minutes}:${seconds}`;
+    if (--timer < 0) {
+      clearInterval(interval);
+    }
+  },
+    1000);
+}
 countdownTimer(20);
 
-    

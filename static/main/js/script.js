f89545ca4efea91a689b43b550c91181d7c53fdb
izzyjosh <calebayuba222@gmail.com>
2024-01-14 12:50:26 +0100

    Making my navigation logic

--- static/main/js/script.js
+++ static/main/js/script.js
@@ -1,3 +1,4 @@
+// countdown timer
 let time = document.querySelector(".time");
 function countdownTimer(durationInSeconds) {
   let timer = durationInSeconds;
@@ -15,3 +16,32 @@ function countdownTimer(durationInSeconds) {
 }
 countdownTimer(20);
 
+
+//quiz logic
+
+document.addEventListener("DOMContentLoaded", function () {
+  var storedFormData = sessionStorage.getItem('formData');
+
+  if (storedFormData) {
+    var formData = JSON.parse(storedFormData);
+
+    document.querySelector('input[name="options"][value="' + formData.selectedOption + '"]').checked = true;
+
+  }
+
+  var formElement = document.getElementById('myForm');
+
+  formElement.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    var selectedOption = document.querySelector('input[name="options"]:checked').value;
+
+    sessionStorage.setItem('formData', JSON.stringify({
+      selectedOption: selectedOption
+    }));
+  console.log("Form submitted with option: " + selectedOption);
+
+  })
+
+
+})
